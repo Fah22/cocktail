4f4046a615456c71b858138211c36e4eb7242dd5
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-01-17 18:28:19 +0200

    Modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
 import React from 'react'
 import { Store } from './store/Store'
-import { Link } from 'react-router-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import { Favorites } from './components/Favorites'
-import useLocalStorage from 'use-local-storage'
 
 const App = (props:any):JSX.Element => {
- 
   const {state, dispatch} = React.useContext(Store)
-
-  const [theme, setTheme] = useLocalStorage<string>('theme' ? 'dark' : 'light')
-
-  const switchTheme = ():void => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme)
-  }
 
   return (
     <>
-      <div className='app' data-theme={theme}>
+      <div className='app'>
         <header className='header'>
           <div>
             <h1>The Bold Type</h1>
           </div>
           <div>
-            <div className='theme-toggle'>
-              <i onClick={switchTheme} className="fas fa-toggle-off"></i>
-            </div>
-            <a href='/'>Home</a>
-            <a href='/favorites'>Favorite(s): {state.favorites.length}</a>
+            <a className='fav' href='/favorites'>Favorite(s): {state.favorites.length}</a>
           </div>
           {props.children}
         </header>
--- src/components/Episodes.tsx
+++ src/components/Episodes.tsx
@@ -9,14 +9,14 @@ export default function Episodes(props: any):JSX.Element[] | any {
     return episodes.map((episode:IEpisode) => {
           return (
             <section key={episode.id} className='episode-box'>
-              <img src={episode.image.medium} alt={`The Bold Type ${episode.name}`} />
+              <img className='episode-img' src={episode.image.medium} alt={`The Bold Type ${episode.name}`} />
               <div>{episode.name}</div>
               <section style={{ display: 'flex', justifyContent: 'space-between'}}>
                 <div>
                   Season: {episode.season} Number: {episode.number}
                 </div>
                 <button type='button' onClick={() => favEpisode(state, dispatch, episode)}>
-                  {favorites.find((fav:IEpisode) => fav.id == episode.id) ? <i className="fas fa-heart"></i> : <i className="far fa-heart"></i>}
+                  {favorites.find((fav:IEpisode) => fav.id == episode.id) ? <i id="heart-fill" className="fas fa-heart"></i> : <i id='heart-outline' className="far fa-heart"></i>}
                 </button>
               </section>
             </section>
