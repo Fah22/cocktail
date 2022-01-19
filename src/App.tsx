fd6b557cf3f4c4e713c8fc90c97386acd003ba19
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-01-19 12:04:01 +0200

    Modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Store } from './store/Store'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
 import { Favorites } from './components/Favorites'
 
@@ -9,27 +9,34 @@ const App = (props:any):JSX.Element => {
 
   return (
     <>
-      <div className='app'>
-        <header className='header'>
-          <div>
-            <h1>The Bold Type</h1>
-          </div>
-          <div>
-            <a className='fav' href='/favorites'>Favorite(s): {state.favorites.length}</a>
-          </div>
-          {props.children}
-        </header>
-      <div>
+    <BrowserRouter>
+        <div className='app'>
+          <header className='header'>
+            <div>
+              <h1>
+                The Bold Type</h1>
+              <p className='fav'>
+                <Link className='fav' to='/favorites'>Favorite(s): {state.favorites.length}
+                </Link>
+                </p>
+            </div>
+            <div>
+              
+            </div>
+            {props.children}
+          </header>
+        <div>
         <h3 className='heading'>Pick your favourite Episode</h3>
-      </div>
-      </div>
-      
-      <BrowserRouter>
-        <Routes>
+        </div>
+        </div>
+      <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/favorites' element={<Favorites />} />
         </Routes>
     </BrowserRouter>
+      
+      
+      
     </>
   )
 }
